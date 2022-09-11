e448ad463e4e8a337a91cffb77e79d2bf796fcc1
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-09-11 16:02:47 +0300

    2nd commit I del /dist replaced cross-env

---
+++ src/example.ts
@@ -0,0 +1,13 @@
+const str = 'string';
+const num = 2;
+const nan = NaN;
+const obj = {};
+const arr = [];
+const nul = null;
+const sym = Symbol();
+const und = undefined;
+const _void = void 0;
+const bool = true;
+const fn = () => { };
+
+let tsStr: string = 'asd';
